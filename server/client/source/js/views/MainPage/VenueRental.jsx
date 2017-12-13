1d52999cf6bd63eadf4ce90b5fd17923170e7371
Dastan <dastan.zhanibek@gmail.com>
2017-12-13 10:32:04 +0800

    add qr code and venue rental images hover function

--- server/client/source/js/views/MainPage/VenueRental.jsx
+++ server/client/source/js/views/MainPage/VenueRental.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import { Row, Col, Image } from 'react-bootstrap';
 
+const CHANGDI = [
+  {
+    name: '',
+    img: 'https://res.cloudinary.com/cacicloud/image/upload/v1510126717/changdi/1_kaap3e.jpg',
+  },
+  {
+    name: '香巴拉',
+    img: 'https://res.cloudinary.com/cacicloud/image/upload/v1510126717/changdi/3_dnyumu.jpg',
+  },
+  {
+    name: '',
+    img: 'https://res.cloudinary.com/cacicloud/image/upload/v1510126716/changdi/2_jmrlyg.jpg',
+  },
+  {
+    name: '香巴拉',
+    img: 'https://res.cloudinary.com/cacicloud/image/upload/v1510126717/changdi/4_tcqqoo.jpg',
+  },
+];
+
 export const VenueRental = () => {
   return (
-    <div className='Section9'>
+    <div className='venue-rental'>
       <section id='m-changdi' className=''>
         <div className='container'>
           <div className='center'>
@@ -11,18 +30,18 @@ export const VenueRental = () => {
             <p className='lead'>Venue Rental</p>
           </div>
           <Row>
-            <Col sm={ 3 } xs={ 6 }>
-              <Image src='https://res.cloudinary.com/cacicloud/image/upload/v1510126717/changdi/1_kaap3e.jpg' circle />
-            </Col>
-            <Col sm={ 3 } xs={ 6 }>
-              <Image src='https://res.cloudinary.com/cacicloud/image/upload/v1510126717/changdi/3_dnyumu.jpg' circle />
-            </Col>
-            <Col sm={ 3 } xs={ 6 }>
-              <Image src='https://res.cloudinary.com/cacicloud/image/upload/v1510126716/changdi/2_jmrlyg.jpg' circle />
-            </Col>
-            <Col sm={ 3 } xs={ 6 }>
-              <Image src='https://res.cloudinary.com/cacicloud/image/upload/v1510126717/changdi/4_tcqqoo.jpg' circle />
-            </Col>
+            {CHANGDI.map((data, index) => (
+              <Col key={ index } sm={ 3 } xs={ 6 }>
+                <div className='venue-rental-wrap'>
+                  <Image src={ data.img } circle />
+                  <div className='overlay'>
+                    <div className='venue-rental-inner'>
+                      <h3>{data.name}</h3>
+                    </div>
+                  </div>
+                </div>
+              </Col>
+            ))}
           </Row>
         </div>
       </section>
