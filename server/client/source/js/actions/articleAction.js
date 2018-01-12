a8fe0f4d5f30d80c64c51a2c271eb367a71f33e4
Dastan <dastan.zhanibek@gmail.com>
2018-01-12 17:11:44 +0800

    update api

--- server/client/source/js/actions/articleAction.js
+++ server/client/source/js/actions/articleAction.js
@@ -1,7 +1,7 @@
 import AppTypes from '../types/appTypes';
 
 export const fetchArticle = () => async dispatch => {
-  const res = await fetch('https://caci-admin.herokuapp.com/admin/api/fetch_article', { mode: 'cors' });
+  const res = await fetch('http://112.126.81.232/admin/api/fetch_article', { mode: 'cors' });
   const json = await res.json();
 
   dispatch({ type: AppTypes.FETCH_ARTICLE, payload: json });
--- server/client/source/js/actions/jobAction.js
+++ server/client/source/js/actions/jobAction.js
@@ -1,7 +1,7 @@
 import AppTypes from '../types/appTypes';
 
 export const fetchJobs = () => async dispatch => {
-  const res = await fetch('https://caci-admin.herokuapp.com/admin/api/fetch_jobs', { mode: 'cors' });
+  const res = await fetch('http://112.126.81.232/admin/api/fetch_jobs', { mode: 'cors' });
   const json = await res.json();
 
   dispatch({ type: AppTypes.FETCH_JOBS, payload: json });
