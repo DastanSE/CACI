8e14155618492c9480dfd0c701c02190d717eedf
DastanSE <dastan.zhanibek@gmail.com>
2018-02-28 11:10:38 +0800

    swipe images on mainpage

--- server/client/source/js/actions/repertoireAction.js
+++ server/client/source/js/actions/repertoireAction.js
@@ -1,7 +1,7 @@
 import AppTypes from '../types/appTypes';
 
 export const fetchRepertoire = () => async dispatch => {
-  const res = await fetch('http://112.126.81.232/admin/api/fetch_repertoire', { mode: 'cors' });
+  const res = await fetch('http://112.126.81.232:5000/admin/api/fetch_repertoire', { mode: 'cors' });
   const json = await res.json();
 
   dispatch({ type: AppTypes.FETCH_REPERTOIRE, payload: json });
